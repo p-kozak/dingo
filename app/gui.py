b98dbf0ec8d00a2c7bed537fa72bfb7fa5b5404f
patkoz98 <patkoz98@gmail.com>
2019-02-20 15:31:51 +0000

    Started implementing GUI

--- app/gui.py
+++ app/gui.py
@@ -4,21 +4,65 @@ from control import Control
 from comms import Comms
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow): 
 	def __init__(self, parent = None):
 		QMainWindow.__init__(self)
+		#Set the properties of the window
+		self.setMinimumSize(800,480)
 		self.setWindowTitle("Dingo is amazing")
 		
 		#test code just to put something on the grid
-		widget = QWidget()
-		layout = QGridLayout()
-		widget.setLayout(layout)
-		self.setCentralWidget(widget)
-		button = QPushButton("dupa")
-		layout.addWidget(button, 0,0)
+		mainWidget = QWidget()
+		mainLayout = QHBoxLayout()
+		mainWidget.setLayout(mainLayout)
+		self.setCentralWidget(mainWidget)
+
+		#Layout and widget for the left part of the GUI - buttons
+		gridWidget = QWidget()
+		gridWidget.setMinimumSize(280,480)
+		gridLayout = QGridLayout()
+		gridWidget.setLayout(gridLayout)
+		mainLayout.addWidget(gridWidget)
 
 
-		#set up threads. set up objects and move them to threads
+
+		#Prototype layout and widget for the right part of the GUI- display
+		horWidget = QWidget()
+		horWidget.setMinimumSize(520,480)
+		horLayout = QVBoxLayout()
+		horWidget.setLayout(horLayout)
+		mainLayout.addWidget(horWidget)
+
+		#Buttons for the left part of the layout
+
+		button = QPushButton("Toggle laser")
+		gridLayout.addWidget(button,0,0)
+
+		button = QPushButton("Bluetooth Connect")
+		gridLayout.addWidget(button,0,1)
+
+		boxDistanceLabel = QLabel("Last distance:")
+		gridLayout.addWidget(boxDistanceLabel,1,0)
+
+		boxAngleLabel = QLabel("Current angle:")
+		gridLayout.addWidget(boxAngleLabel,1,1)
+
+		boxDistanceLabel = QLabel("Last distance:")
+		gridLayout.addWidget(boxDistanceLabel,1,0)
+
+		boxAngleLabel = QLabel("Current angle:")
+		gridLayout.addWidget(boxAngleLabel,1,1)
+
+
+
+
+
+
+		
+		
+
+
+		#Set up threads. Initialise objects and move them to threads
 		self.controlThread = QThread()
 		self.commsThread = QThread()
 
@@ -32,21 +76,48 @@ class MainWindow(QMainWindow):
 		self.commsThread.start()
 
 
-
-
-
 		self.show()
 
 
 
 
 	def __del__(self):
-		#initialise closing the thread, wait for it to finish
+		#Destructor. Initialise closing the thread, wait for it to finish, continue
 		self.controlThread.quit()
 		self.controlThread.wait()
 		self.commsThread.quit()
 		self.controlThread.wait()
 
+
+
+	def buttonMeasureClicked():
+		#A slot which handles Measure button click 
+		return
+
+	def buttonSetRelativeAngleToZeroClicked():
+		#Slot which set  relative angle to zero degrees. Useful for calibration
+		return
+
+
+	def buttonMoveRightPressed():
+		#Slot  
+		return
+
+	def buttonMoveRightReleased():
+		#Slot
+		return
+
+	def buttonMoveLeftPressed():
+		#Slot  
+		return
+
+	def buttonMoveLeftReleased():
+		#Slot
+		return
+
+	def getDistanceBetweenTwoLastPointsPressed():
+		#Slot. Uses two last measurements and returns distance between these points  
+		return
 
 
 
