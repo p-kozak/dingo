d6cf4b7559e832924f265f6303049ae301118599
7TreeOwls <olaf.czerwony@gmail.com>
2019-02-21 20:30:54 +0000

    Completed pseudocode

--- pseudocode/Control.py
+++ pseudocode/Control.py
@@ -1,31 +1,36 @@
 #Pseudocode for Control module of software
 
-class point(valuepassed, anglepassed):
+class Point:
     """
     Class used for storing points in polar coordinates.
     Angle is in respect to the front of the device.
     """
-    self.value = valuepassed
-    self.angle = anglepassed
+
+    def __init__(self, valuepassed=0, anglepassed=0, errorpassed=0):
+        self.value = valuepassed
+        self.angle = anglepassed
+        self.error = errorpassed
     
-    def getCartesian()
+    def getCartesian(self)
         """
         Returns the cartesian coordinates of the point.
         """
         return (x, y)
 
-class Map(listofcartesianpoints = []):
+class Map:
     """
     Class used to store list of points in cartesian coordinates, where the device is an origin.
     """
+    def __init__(self, listofcartesianpoints = []):
+        self.pointslits = listofcartesianpoints
 
-    def createMap(listofcartesianpoints)
+    def createMap(self, listofcartesianpoints)
         """
         Creates a proper map with straight walls from given list.
         """
         return mappedPoints
 
-    def getQImage(scale = 0) //TODO decide on default value based on the size of the screen
+    def getQImage(self, scale = 0) //TODO decide on default value based on the size of the screen
         """
         Returns scaled QImage of the map
         """
@@ -33,10 +38,16 @@ class Map(listofcartesianpoints = []):
 
 
 
-class Control():
+class Control(QObject):
     """
     One-instance class that handles processing of data and hardware interaction.
     """
+    
+    def __init__(self):
+        self.angleabs = 0
+        self.anglecurrent = 0
+        self.prevpoint = Point()
+        self.prevprecpoint = Point()
 
     def getLidar():
         """
@@ -53,15 +64,68 @@ class Control():
     def getDistance(samplesize=10)
         """
         takes n = 10 ( //TODO specify appropiate number)
-        measurements and averages them.
-        Returns the averaged distance.
+        measurements and returns them as a list
         """
         l = []
         for it in range(samplesize):
             l[it] = getLidar()
-        
-        averagedist = sum(l) / samplesize
-        return averagedist
+        return l
 
+    def calibrateMotor(self)
+    """Sets current absolute angle to 0"""
+    self.angleabs = 0
+    self.anglecurrent = 0
+    return
+    
+    def getWidth(A, B)
+    """
+    Accepts class point as arguments
+    Calculates the distance between 2 points and error of the calculation.
+    Returns both values.
+    """
+    return width, errorwidth
+
+    def analyseMeasurement(list = [])
+    """
+    Performs mathematical analysis of set of measuerent.
+    Returns calculated value and its error.
+    """
+    return (value, errorvalue)
     
 
+    def defineSignals(self):
+		return
+
+	#These are slots which receive from engine
+	def toggleLaser(self):
+        """Toggles the laser on and off"""		
+		return 
+
+	def moveRightStart(self):
+        """Starts movement of motor to the right until moveStop() is called"""
+		return
+
+	def moveRightStop(self):
+        """Stops movement of motor"""
+		return
+
+	def moveLeftStart(self):
+        """Starts movement of motor to the left until moveStop() is called"""
+		return
+
+	def moveLeftStop(self):
+        """Stops movement of motor"""
+		return
+
+	def measureDistance(self):
+        """Measures distance to the object"""
+        d = getDistance()
+		return
+
+	def setAngleToZero(self):
+        """Calibrates absolute angle to be set at the current position of the motor"""
+		return
+
+	def calculateWidth(self):
+        """Calls getWidth() which returns distanmce bewteen last 2 measured points"""
+		return 
