d32584ad5a235eb416a00d118c5eb381c20c8f89
7TreeOwls <olaf.czerwony@gmail.com>
2019-02-26 15:01:46 +0000

    update

--- app/testmotor.py
+++ app/testmotor.py
@@ -2,16 +2,15 @@ import hardwarecontrol
 
 motor = StepMotor()
 cont = True
-while(cont = True)
-{
+while(cont = True):
     print("Specify number of steps. 1 step = 0.9 degrees")
     innum = input("Specify number of steps, only integers. 1 step = 0.9 degrees.")
     stepnum = int(innum)
 
     print("Moved")
+    motor.turnbystep(stepnum)
     val = input("Do you want to continue? (y/n)")
     if(val == y):
         cont = True
     else:
         cont = False
-}
