21fc723a5f23dcc3f41b21702f3089e8d7decf5b
7TreeOwls <olaf.czerwony@gmail.com>
2019-03-04 10:31:01 +0000

    Test for motor precision implemented

--- app/testmotor.py
+++ app/testmotor.py
@@ -3,7 +3,10 @@ from hardwarecontrol import HardwareControl
 hardware = HardwareControl()
 cont = True
 while cont == True:
-    print("Specify number of steps, only integers. 1 step = 0.05625 degrees. \n If you want to set current angle to absolute 0 input: cal \n If you want to exit input: exit")    
+    print("Specify number of steps, only integers. 1 step = 0.05625 degrees.\
+         \n If you want to set current angle to absolute 0, input: cal\
+         \n If you want to toggle the laser, input: l\
+         \n If you want to exit input: exit")    
     innum = input(">> ")
 
     if innum == "exit":
@@ -11,7 +14,11 @@ while cont == True:
     elif innum == "cal":
         hardware.calibrateMotor()
         print("Calibrated")
+    elif innum == 'l':
+        hardware.toggleLaser()
     else:
         stepnum = int(innum)
+        if hardware.Laser.value == 1:
+            hardware.Laser.off()
         hardware.turnMotor(stepnum, True)
         print("Moved")
